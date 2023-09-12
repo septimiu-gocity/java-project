cf6cd397ca32edfb601ee31b52de6c7ce39f351a
Septimiu-Iulian Calin <septimiu.calin@gocity.com>
2023-09-12 15:45:08 +0100

    solved first challenge: Mortgage calculator

--- src/com/codewithsepti/Main.java
+++ src/com/codewithsepti/Main.java
@@ -1,19 +1,31 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 package com.codewithsepti;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = Integer.parseInt(scanner.nextLine().trim());
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Annual interest rate (%): ");
+        double interestRate = Double.parseDouble(scanner.nextLine().trim()) / 100;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Period (years): ");
+        int period = Integer.parseInt(scanner.nextLine().trim());
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        System.out.print("Mortgage: " + currency.format(getMortgage(principal, interestRate, period)));
+
+    }
+
+    public static double getMortgage(int principal, double interestRate, int period) {
+        double monthlyInterestRate = interestRate / 12;
+        int periodMonths = period * 12;
+        return principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, periodMonths )) / (Math.pow(1 + monthlyInterestRate, periodMonths ) - 1);
+
     }
 }
