e8bae27beccc127cddd65563d6818e632be737e2
Septimiu-Iulian Calin <septimiu.calin@gocity.com>
2023-09-12 16:24:54 +0100

    finished initial java coutse

--- src/com/codewithsepti/Main.java
+++ src/com/codewithsepti/Main.java
@@ -6,6 +6,10 @@ import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
+//      mortgageChallenge();
+        fizzBuzzChallenge();
+    }
+    public static void mortgageChallenge() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal: ");
         int principal = Integer.parseInt(scanner.nextLine().trim());
@@ -18,7 +22,6 @@ public class Main {
 
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         System.out.print("Mortgage: " + currency.format(getMortgage(principal, interestRate, period)));
-
     }
 
     public static double getMortgage(int principal, double interestRate, int period) {
@@ -27,4 +30,22 @@ public class Main {
         return principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, periodMonths )) / (Math.pow(1 + monthlyInterestRate, periodMonths ) - 1);
 
     }
+
+    public static void fizzBuzzChallenge() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("FizzBuzz up to: ");
+        int fizzBuzzLimit = Integer.parseInt(scanner.nextLine().trim());
+
+
+        for (int i = 1; i <= fizzBuzzLimit; i++) {
+            String output = "";
+            if (i % 3 == 0) output += "Fizz";
+            if (i % 5 == 0) output += "Buzz";
+
+            if (output.isEmpty()) output += Integer.toString(i);
+
+            System.out.println(output);
+        }
+    }
+
 }
